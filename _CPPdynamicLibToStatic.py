41bde7873c4e9601e6749b35173cec09f29c0794
Mikhail Kuznetsov <61430641+Mike-Kuznetsov@users.noreply.github.com>
2022-06-22 20:09:12 +0300

    Add files via upload

---
+++ _CPPdynamicLibToStatic.py
@@ -0,0 +1,16 @@
+import os
+for filename in os.listdir(os.getcwd()):
+    with open(os.path.join(os.getcwd(), filename), 'r+') as f: # Read every file in folder
+        if (filename!="_CPPdynamicLibToStatic.py"): # If we read file which is not this program file
+            data=f.read();
+            result="";
+            for line in data.splitlines(): # Go through every line
+                if "<openssl/" in line: # If library initialization in line 
+                    line=line.replace("<openssl/",'"'); # Replace path to lib to current folder
+                    line=line.replace(">",'"');
+                    #print(line);
+                result = result+"\n"+line;
+            f.truncate(0); # Clear file
+            f.write(result); # Write new data to file
+        f.close();
+               
